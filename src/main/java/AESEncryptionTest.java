26216ae59679f7f3fec6bb604914df1d7cdbd5f8
tobesoft <jinu712@gmail.com>
2022-03-30 17:07:00 +0900

    Initial commit

---
+++ src/main/java/AESEncryptionTest.java
@@ -0,0 +1,46 @@
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+public class AESEncryptionTest {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException
+            , InvalidKeyException, IllegalBlockSizeException
+            , UnsupportedEncodingException, BadPaddingException {
+
+        // 암호화에 사용할 키, 디폴트로 128bit(16Byte)
+        String encryptionKey = "SomeText!";
+
+        // 암호화할 문자열
+        String target = "AESJavaEncrypt";
+
+
+        // AES로 암호화 =================================================
+
+        // AES Cipher 객체 생성
+        Cipher cipher = null;
+        try {
+            cipher = Cipher.getInstance("AES");
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        }
+
+        // 암호화 Chipher 초기화
+        SecretKeySpec secretKeySpec = new SecretKeySpec(encryptionKey.getBytes(),"AES");
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+
+        // 암호화 완료
+        byte[] encryptBytes = cipher.doFinal(target.getBytes("UTF-8"));
+        System.out.println(new String(encryptBytes)); // => 똑같은 암호화키로 복호화
+
+
+        // AES로 복호화 =================================================
+
+        // 복호화 Chipher 초기화, 똑같은 암호화키로 복호화
+        cipher.init(cipher.DECRYPT_MODE, secretKeySpec);
+        byte[] decryptBytes = cipher.doFinal(encryptBytes);
+        System.out.println(new String(decryptBytes, "UTF-8"));
+    }
+}
---
+++ src/main/java/RSAEncryptionTest.java
@@ -0,0 +1,47 @@
+import java.security.InvalidKeyException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+
+public class RSAEncryptionTest {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException
+            , NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException
+            , BadPaddingException {
+
+        // 암호화할 문자열
+        String data = "SomeText";
+
+
+        // RSA 로 암호화 =================================================
+
+        // RSA 비밀키와 공개키를 생성
+        KeyPairGenerator keypairgen = KeyPairGenerator.getInstance("RSA");
+        KeyPair keyPair = keypairgen.generateKeyPair();
+        RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
+        RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
+
+        // Cipher 객체 생성과 비밀키 초기화
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+
+        // 암호화 완료
+        byte[] encryptBytes = cipher.doFinal(data.getBytes());
+        System.out.println(new String(encryptBytes)); // => 암호화되어 읽지못함
+
+        // RSA로 복호화 =================================================
+
+        // 복호화 Chipher 초기화, 비밀키와 쌍인 공개키로 복호화함.
+        cipher.init(cipher.DECRYPT_MODE, publicKey);
+        byte[] decryptBytes = cipher.doFinal(encryptBytes);
+        System.out.println(new String(decryptBytes));
+    }
+
+}
